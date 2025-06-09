d1ef626dbae969cfc63fb21d48344c0536d5d21c
Francesco Sardone <francesco@airscript.it>
2025-06-09 14:30:16 +0000

    feat: add base seed file

---
+++ supabase/seed.sql
@@ -0,0 +1,39 @@
+INSERT INTO "github"."repositories" (
+    "id",
+    "username",
+    "repository",
+    "url",
+    "stars",
+    "forks",
+    "created_at",
+    "updated_at",
+    "deleted_at"
+) VALUES (
+    'f3aa4531-cfb6-42ff-921f-095bf85d11e8',
+    'supabase',
+    'supabase',
+    'https://github.com/supabase/supabase',
+    '80000',
+    '8000',
+    '2025-05-27 15:52:23.561993+00',
+    '2025-05-27 15:52:23.561993+00',
+    NULL
+);
+
+INSERT INTO "github"."profiles" (
+    "id",
+    "username",
+    "url",
+    "followers",
+    "created_at",
+    "updated_at",
+    "deleted_at"
+) VALUES (
+    'c46aaa96-474e-4498-9331-da5d1a93c9f8',
+    'antirez',
+    'https://github.com/antirez',
+    '23000',
+    '2025-06-06 23:20:10.130589+00',
+    '2025-06-06 23:20:10.130589+00',
+    NULL
+);
